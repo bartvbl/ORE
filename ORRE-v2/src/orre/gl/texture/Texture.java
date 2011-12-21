e8e7b0e9b75874c17213693a4bd832426c720273
Bart van Blokland <bartiver@stud.ntnu.no>
2011-12-21 19:14:07 +0100

    small fix in Texture.blit()

--- ORRE-v2/src/orre/gl/texture/Texture.java
+++ ORRE-v2/src/orre/gl/texture/Texture.java
@@ -3,7 +3,7 @@ package orre.gl.texture;
 import static org.lwjgl.opengl.GL11.*;
 
 public class Texture {
-	public int textureReference = -1;
+	private final int textureReference;
 	
 	public Texture(int textureReference, int width, int height)
 	{
@@ -13,5 +13,20 @@ public class Texture {
 	public void bind()
 	{
 		glBindTexture(GL_TEXTURE_2D, this.textureReference);
+	}
+	
+	public void blit(int x, int y, int width, int height)
+	{
+		this.bind();
+		glBegin(GL_QUADS);
+		glTexCoord2f(0,0);
+		glVertex2f(x,y);
+		glTexCoord2f(1,0);
+		glVertex2f(x+width,y);
+		glTexCoord2f(1,1);
+		glVertex2f(x+width,y+height);
+		glTexCoord2f(0,1);
+		glVertex2f(x,y+height);
+		glEnd();
 	}
 }
