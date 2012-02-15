b0fc2d47191d94f6d386b08834c43d99129bc7e8
Bart van Blokland <bartiver@stud.ntnu.no>
2012-02-15 15:57:35 +0100

    FTFY :)

--- ORRE-v2/src/openrr/test/TextButton.java
+++ ORRE-v2/src/openrr/test/TextButton.java
@@ -2,25 +2,19 @@ package openrr.test;
 
 import static org.lwjgl.opengl.GL11.*;
 
-import java.util.ArrayList;
-
 import orre.gl.RenderUtils;
-import orre.gl.texture.Texture;
-import orre.resources.loaders.TextureLoader;
-
 import java.awt.Font;
-import java.io.FileNotFoundException;
-import java.io.FileInputStream;
 
-import org.lwjgl.util.Rectangle;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.font.effects.ColorEffect;
 
 public class TextButton extends Button {
 	
-	String text;
-	UnicodeFont font;
+	private String text;
+	private UnicodeFont font;
+	private Color buttonTextColour = Color.black;
 	
 	public TextButton(int x, int y, int screenSize[], String inAlign, String inText) {
 		super(x, y, screenSize, inAlign);
@@ -43,11 +37,11 @@ public class TextButton extends Button {
 		state = newState;
 		if (state==HOVER) {
 			text = "BLARGH";
-			font.getEffects().add(new ColorEffect(java.awt.Color.BLUE)); 
+			this.buttonTextColour = Color.white;
 		}
 		else {
-			font.getEffects().add(new ColorEffect(java.awt.Color.WHITE)); 
 			text = "TEST";
+			this.buttonTextColour = Color.blue;
 		}
 	}
 	
@@ -56,6 +50,7 @@ public class TextButton extends Button {
 		glDisable(GL_CULL_FACE);
 		glTranslatef(0f, 24 - 2*-y, 0f);//24 = font size (I think)
 		glScalef(1, -1, 1);
-		font.drawString(x, y, text);
+		font.drawString(x, y, text, this.buttonTextColour);
+		glColor4f(1, 1, 1, 1);
 	}
 }
