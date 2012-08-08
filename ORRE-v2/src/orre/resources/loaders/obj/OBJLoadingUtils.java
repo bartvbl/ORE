537577f453b74723caca9e63b4088350ae61dd55
Bart van Blokland <bartiver@stud.ntnu.no>
2012-08-08 11:33:05 +0200

    sync commit

--- ORRE-v2/src/orre/resources/loaders/obj/OBJLoadingUtils.java
+++ ORRE-v2/src/orre/resources/loaders/obj/OBJLoadingUtils.java
@@ -5,7 +5,7 @@ import orre.geom.vbo.BufferDataFormatType;
 public class OBJLoadingUtils {
 	public static float[] parseFloatLine(String line) {
 		String[] parts = line.split(" ");
-		float[] floatLine = new float[parts.length-1];//the parts array includes the line identifier (f.ex. v, vt, vn, etc) 
+		float[] floatLine = new float[parts.length-1];//the array from the split string includes the line identifier (f.ex. v, vt, vn, etc) 
 		for(int i = 1; i < parts.length; i++)
 		{
 			floatLine[i-1] = Float.parseFloat(parts[i]);
