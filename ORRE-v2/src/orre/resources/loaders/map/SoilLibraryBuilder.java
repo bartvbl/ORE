dbcd69d13f8ab3ee0acf44c5378cdbb6f31ca481
Bart van Blokland <bartiver@stud.ntnu.no>
2012-11-13 12:24:05 +0100

    not very significant changes. Updated the map file and started on the soil map loader

---
+++ ORRE-v2/src/orre/resources/loaders/map/SoilLibraryBuilder.java
@@ -0,0 +1,14 @@
+package orre.resources.loaders.map;
+
+import java.util.ArrayList;
+
+import openrr.map.Soil;
+
+public class SoilLibraryBuilder {
+
+	public static ArrayList<Soil> buildSoilLibrary(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- ORRE-v2/src/orre/resources/loaders/map/SoilMapLoader.java
+++ ORRE-v2/src/orre/resources/loaders/map/SoilMapLoader.java
@@ -1,5 +1,7 @@
 package orre.resources.loaders.map;
 
+import java.util.ArrayList;
+
 import nu.xom.Element;
 import openrr.map.Soil;
 
@@ -7,6 +9,7 @@ public class SoilMapLoader {
 
 	public static Soil[][] loadSoilMap(Element mapDefinitionElement, int width, int height) {
 		//1. load soil library
+		ArrayList<Soil> soilLibrary = SoilLibraryBuilder.buildSoilLibrary("res/");
 		//2. load custom tiles
 		//3. load soil map
 		return null;
