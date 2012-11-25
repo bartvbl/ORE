84f8df00f163259157cbf0e399eabfd3e47da1a6
Bart van Blokland <bartiver@stud.ntnu.no>
2012-11-25 13:06:08 +0100

    got rid of the chunk references. That code has to be rewritten

--- ORRE-v2/src/openrr/map/Map.java
+++ ORRE-v2/src/openrr/map/Map.java
@@ -1,21 +1,21 @@
 package openrr.map;
 
 import orre.resources.ResourceCache;
+import orre.sceneGraph.EmptySceneNode;
 import orre.sceneGraph.SceneNode;
 
 public class Map {
 	
-	private final ChunkCache chunkCache;
-
+	
 	public Map(MapTile[][] tileMap) {
-		this.chunkCache = ChunkCacheBuilder.buildChunkCache(tileMap, tileMap.length, tileMap[0].length);
+		
 	}
 
 	public SceneNode createSceneNode() {
-		return chunkCache.createSceneNode();
+		return new EmptySceneNode();
 	}
 	
 	public void tick() {
-		this.chunkCache.update();
+		
 	}
 }
