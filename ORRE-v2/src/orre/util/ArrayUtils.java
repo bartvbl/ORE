89b9c5757c4c86c1c29bf8ead6740ee974d22bf9
Bart van Blokland <bartiver@stud.ntnu.no>
2014-04-30 00:56:30 +0200

    Fixed up a dumb implementation

--- ORRE-v2/src/orre/util/ArrayUtils.java
+++ ORRE-v2/src/orre/util/ArrayUtils.java
@@ -2,15 +2,9 @@ package orre.util;
 
 public class ArrayUtils {
 	public static void shiftLeft(Object[] array, int amount) {
-		Object[] tempArray = new Object[amount];
-		for(int i = 0; i < amount; i++) {
-			tempArray[i] = array[i];
-		}
-		for(int i = 0; i < array.length - amount; i++) {
-			array[i] = array[i + amount];
-		}
-		for(int i = 0; i < amount; i++) {
-			array[array.length - amount + i] = tempArray[i];
+		Object[] temp = array.clone();
+		for(int i = 0; i < array.length; i++) {
+			array[(i - amount + array.length) % array.length] = temp[i];
 		}
 	}
 }
