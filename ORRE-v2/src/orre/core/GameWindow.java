b62b051c6d13b8c53e32deee16aef70a43ae12ff
Bart van Blokland <bartiver@stud.ntnu.no>
2014-05-29 10:27:05 +0200

    Moved openGL intialisation code

--- ORRE-v2/src/orre/core/GameWindow.java
+++ ORRE-v2/src/orre/core/GameWindow.java
@@ -9,6 +9,7 @@ import java.nio.ByteBuffer;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
+import orre.gl.RenderUtils;
 import orre.resources.loaders.TextureLoader;
 
 public class GameWindow {
@@ -35,7 +36,7 @@ public class GameWindow {
 	private static void doInitialization(String gameName) throws Exception
 	{
 		createDisplay(gameName);
-		initOpenGL();
+		RenderUtils.initOpenGL(DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
 	}
 	private static void createDisplay(String gameName) throws Exception
 	{
@@ -46,20 +47,5 @@ public class GameWindow {
 		Display.setIcon(new ByteBuffer[]{TextureLoader.getImageData(new File("res/icon.png")), TextureLoader.getImageData(new File("res/icon.png"))});
 		System.out.println("-- \""+gameName+"\" on ORRE v0.01 (java " + System.getProperty("java.version") + " running on " + System.getProperty("os.name") + " (" + System.getProperty("os.version") + ", "+System.getProperty("os.arch")+")) --");
 		Display.create();
-	}
-	private static void initOpenGL()
-	{
-		glViewport(0, 0, GameWindow.DEFAULT_WINDOW_WIDTH, GameWindow.DEFAULT_WINDOW_HEIGHT);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		gluPerspective(60.0f, (float)(GameWindow.DEFAULT_WINDOW_WIDTH/GameWindow.DEFAULT_WINDOW_HEIGHT), 0.1f, 10000.0f);
-		glMatrixMode(GL_MODELVIEW);
-		glClearColor(0, 0, 0, 1);
-		glClearDepth(1.0);
-		glShadeModel(GL_SMOOTH);
-		glEnable(GL_DEPTH_TEST);
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		glEnable(GL_LIGHTING);
 	}
 }
--- ORRE-v2/src/orre/gl/RenderUtils.java
+++ ORRE-v2/src/orre/gl/RenderUtils.java
@@ -6,6 +6,8 @@ import static org.lwjgl.util.glu.GLU.gluPerspective;
 
 import org.lwjgl.opengl.Display;
 
+import orre.core.GameWindow;
+
 public class RenderUtils {
 	public static final float NEAR_POINT = 0.1f;
 	public static final float FAR_POINT = 250f;
@@ -44,6 +46,22 @@ public class RenderUtils {
 		glEnable(GL_TEXTURE_2D);
 		glDisable(GL_LIGHTING);
 		glDisable(GL_CULL_FACE); 
+	}
+	
+	public static void initOpenGL(int windowWidth, int windowHeight)
+	{
+		glViewport(0, 0, GameWindow.DEFAULT_WINDOW_WIDTH, GameWindow.DEFAULT_WINDOW_HEIGHT);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		gluPerspective(60.0f, (float)windowWidth / (float)windowHeight, 0.1f, 10000.0f);
+		glMatrixMode(GL_MODELVIEW);
+		glClearColor(0, 0, 0, 1);
+		glClearDepth(1.0);
+		glShadeModel(GL_SMOOTH);
+		glEnable(GL_DEPTH_TEST);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+		glEnable(GL_LIGHTING);
 	}
 
 	public static void resetSettings() {
