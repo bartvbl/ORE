57a145de499e8832f5072c854cda4c69bf5cd58a
Bart van Blokland <bartiver@stud.ntnu.no>
2014-05-31 23:56:50 +0200

    Fixed compiler error

--- ORRE-v2/src/orre/gameStates/MainMenu.java
+++ ORRE-v2/src/orre/gameStates/MainMenu.java
@@ -3,8 +3,6 @@ package orre.gameStates;
 import java.io.File;
 import java.nio.FloatBuffer;
 
-import openrr.test.LightTestClass;
-
 import org.lwjgl.BufferUtils;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
