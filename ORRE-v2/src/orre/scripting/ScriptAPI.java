7ad197d53f019923bec579013f4f333315201ca7
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-02 20:09:41 +0200

    Updated scripting

--- ORRE-v2/src/orre/scripting/ScriptAPI.java
+++ ORRE-v2/src/orre/scripting/ScriptAPI.java
@@ -1,12 +1,32 @@
 package orre.scripting;
 
-public class ScriptAPI {
+import orre.events.EventHandler;
+import orre.events.GlobalEvent;
+import orre.events.GlobalEventDispatcher;
+import orre.events.GlobalEventType;
+import orre.threads.ScriptExecutionThread;
+
+public class ScriptAPI implements EventHandler {
+	private final ScriptExecutionThread executionThread;
+
+	public ScriptAPI(GlobalEventDispatcher eventDispatcher, ScriptExecutionThread scriptExecutionThread) {
+		eventDispatcher.addEventListener(this, GlobalEventType.CHANGE_GAME_STATE);
+		this.executionThread = scriptExecutionThread;
+	}
+
 	public static void spawn() {
 		
 	}
 	
 	public void on() {
 		
+	}
+
+	@Override
+	public void handleEvent(GlobalEvent<?> event) {
+		if(event.eventType == GlobalEventType.CHANGE_GAME_STATE) {
+			
+		}
 	}
 	
 	
--- ORRE-v2/src/orre/threads/ScriptExecutionThread.java
+++ ORRE-v2/src/orre/threads/ScriptExecutionThread.java
@@ -2,12 +2,16 @@ package orre.threads;
 
 import org.python.util.PythonInterpreter;
 
+import orre.core.GameMain;
 import orre.events.GlobalEventDispatcher;
 import orre.gameWorld.core.GameWorld;
+import orre.scripting.ScriptAPI;
+import orre.util.Queue;
 
 public class ScriptExecutionThread extends Thread {
 	private final PythonInterpreter interpreter;
 	private final GlobalEventDispatcher eventDispatcher;
+	private final Queue<String> executionQueue = new Queue<String>();
 	
 	private boolean hasUpdatedWorld = false;
 	private GameWorld currentWorld = null;
@@ -15,14 +19,23 @@ public class ScriptExecutionThread extends Thread {
 	public ScriptExecutionThread(GlobalEventDispatcher eventDispatcher) {
 		this.eventDispatcher = eventDispatcher;
 		this.interpreter = new PythonInterpreter();
+		new ScriptAPI(eventDispatcher, this);
 	}
 	
 	public void run() {
+		interpreter.execfile(GameMain.class.getClassLoader().getResourceAsStream("init.py"));
 		while(true) {
 			if(hasUpdatedWorld) {
 				interpreter.set("ORRE_world", currentWorld);
 				hasUpdatedWorld = false;
 			}
+			
+			synchronized(executionQueue) {
+				while(!executionQueue.isEmpty()) {
+					interpreter.exec(executionQueue.dequeue());
+				}
+			}
+				
 			sleep();
 		}
 	}
@@ -36,7 +49,9 @@ public class ScriptExecutionThread extends Thread {
 	}
 	
 	public void execute(String pythonScript) {
-		
+		synchronized(executionQueue) {
+			this.executionQueue.enqueue(pythonScript);
+		}
 	}
 
 	public void setCurrentWorld(GameWorld world) {
