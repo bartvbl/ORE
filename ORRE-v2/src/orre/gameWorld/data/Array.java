be88d0c98bdb3c02f17cb3c4efb3a5755a8161ae
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-10 20:43:29 +0200

    Added some array wrappers so they can be stored in PropertyData

---
+++ ORRE-v2/src/orre/gameWorld/data/Array.java
@@ -0,0 +1,9 @@
+package orre.gameWorld.data;
+
+public class Array<DataType> {
+	public final DataType[] array;
+	
+	public Array(DataType[] array) {
+		this.array = array;
+	}
+}
---
+++ ORRE-v2/src/orre/gameWorld/data/DoubleArray.java
@@ -0,0 +1,9 @@
+package orre.gameWorld.data;
+
+public class DoubleArray<DataType> {
+	public final DataType[][] array;
+	
+	public DoubleArray(DataType[][] array) {
+		this.array = array;
+	}
+}
