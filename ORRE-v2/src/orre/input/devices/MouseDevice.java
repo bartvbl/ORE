f1153129ae0ad2fbd3512a7cff959be1455b012c
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-14 13:44:00 +0200

    Implemented the mouse device

---
+++ ORRE-v2/src/orre/input/devices/MouseDevice.java
@@ -0,0 +1,72 @@
+package orre.input.devices;
+
+import org.lwjgl.input.Mouse;
+
+import orre.input.CommandDispatcher;
+import orre.input.InputEvent;
+import orre.input.KeyType;
+
+public class MouseDevice {
+
+	private final CommandDispatcher dispatcher;
+	private final boolean[] mouseButtonStates;
+	private int mouseWheelPosition = 0;
+
+	public MouseDevice(CommandDispatcher dispatcher) {
+		this.dispatcher = dispatcher;
+		this.mouseButtonStates = new boolean[Mouse.getButtonCount()];
+	}
+
+	public void update() {
+		updateMousePosition();
+		updateMouseButtons();
+		updateMouseWheel();
+	}
+
+	private void updateMousePosition() {
+		final double mouseDX = Mouse.getDX();
+		final double mouseDY = Mouse.getDY();
+		final double mouseX = Mouse.getX();
+		final double mouseY = Mouse.getY();
+		
+		if(mouseDX != 0) {
+			dispatcher.dispatchCommand(new InputEvent(KeyType.MOUSE_X, mouseX, mouseDX));
+		}
+		if(mouseDY != 0) {
+			dispatcher.dispatchCommand(new InputEvent(KeyType.MOUSE_Y, mouseY, mouseDY));
+		}
+	}
+
+	private void dispatchButtonChange(int buttonIndex, boolean buttonState) {
+		double buttonDelta = buttonState ? 1.0 : -1.0;
+		double buttonValue = buttonState ? 1.0 : 0.0;
+		switch(buttonIndex) {
+		case 0:
+			dispatcher.dispatchCommand(new InputEvent(KeyType.MOUSE_LEFT_BUTTON, buttonValue, buttonDelta));
+			return;
+		case 1:
+			dispatcher.dispatchCommand(new InputEvent(KeyType.MOUSE_RIGHT_BUTTON, buttonValue, buttonDelta));
+			return;
+		case 2:
+			dispatcher.dispatchCommand(new InputEvent(KeyType.MOUSE_MIDDLE_BUTTON, buttonValue, buttonDelta));
+			return;
+		}
+	}
+	
+	private void updateMouseButtons() {
+		while(Mouse.next()) {
+			int buttonIndex = Mouse.getEventButton();
+			boolean buttonState = Mouse.getEventButtonState();
+			mouseButtonStates[buttonIndex] = buttonState;
+			dispatchButtonChange(buttonIndex, buttonState);
+		}
+	}
+
+	private void updateMouseWheel() {
+		final int mouseWheelDelta = Mouse.getDWheel();
+		if(mouseWheelDelta != 0) {
+			mouseWheelPosition += mouseWheelDelta;
+			dispatcher.dispatchCommand(new InputEvent(KeyType.MOUSE_WHEEL, mouseWheelPosition, mouseWheelDelta));
+		}
+	}
+}
