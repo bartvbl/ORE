c4dc3e379bf1c2800be331d864311e7db84073c0
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-21 13:05:16 +0200

    Structure update of the image element

---
+++ ORRE-v2/src/orre/gui/elementNodes/ImageNode.java
@@ -0,0 +1,16 @@
+package orre.gui.elementNodes;
+
+import orre.gui.baseNodes.GUIBaseNode;
+
+public class ImageNode extends GUIBaseNode {
+	
+	public ImageNode() {
+		
+	}
+
+	@Override
+	protected void draw(double x1, double y1, double x2, double y2) {
+		
+	}
+
+}
--- ORRE-v2/src/orre/gui/elements/ImageElement.java
+++ ORRE-v2/src/orre/gui/elements/ImageElement.java
@@ -1,14 +1,14 @@
 package orre.gui.elements;
 
 import orre.gui.Bounds;
-import orre.sceneGraph.CoordinateNode;
+import orre.gui.elementNodes.ImageNode;
 
 public class ImageElement extends GUIElement {
 
 	private final String imageName;
 
 	public ImageElement(Bounds bounds, String name, String imageName) {
-		super(bounds, new CoordinateNode(), name);
+		super(bounds, new ImageNode(), name);
 		this.imageName = imageName;
 	}
 
