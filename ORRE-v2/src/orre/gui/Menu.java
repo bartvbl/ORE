7fa41a2b100cf27f277726d94a0c72dc7ed63a99
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-21 13:03:40 +0200

    Menus now update the bounds of the GUI elements they house.

--- ORRE-v2/src/orre/gui/Menu.java
+++ ORRE-v2/src/orre/gui/Menu.java
@@ -1,8 +1,12 @@
 package orre.gui;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.lwjgl.opengl.Display;
+
 import orre.animation.Animatable;
+import orre.gui.controls.Control;
 import orre.gui.elements.Container;
 import orre.gui.elements.GUIElement;
 import orre.sceneGraph.CoordinateNode;
@@ -12,6 +16,7 @@ public class Menu implements Animatable {
 	public final Container root;
 	
 	private final HashMap<String, GUIElement> registeredElements = new HashMap<String, GUIElement>();
+	private final ArrayList<Control> menuControls = new ArrayList<Control>();
 
 	public Menu(String name, Container root) {
 		this.name = name;
@@ -20,10 +25,36 @@ public class Menu implements Animatable {
 	
 	public void registerGUIElement(GUIElement element) {
 		this.registeredElements.put(element.name, element);
+		if(element instanceof Control) {
+			this.menuControls.add((Control) element);
+		}
+	}
+	
+	public void setAnimationActive(boolean isActive) {
+		
 	}
 
 	@Override
 	public CoordinateNode getModelPartByName(String name) {
 		return registeredElements.get(name).sceneNode;
+	}
+
+	public void update(double mouseX, double mouseY, boolean mouseState) {
+		updateBounds(root, Display.getWidth(), Display.getHeight());
+		for(Control control : menuControls) {
+			control.updateMouse(mouseX, mouseY, mouseState);
+		}
+	}
+	
+	private void updateBounds(GUIElement element, double parentWidth, double parentHeight) {
+		double x = element.bounds.getX(parentWidth, parentHeight);
+		double y = element.bounds.getY(parentWidth, parentHeight);
+		double width = element.bounds.getWidth(parentWidth, parentHeight);
+		double height = element.bounds.getHeight(parentWidth, parentHeight);
+		element.sceneNode.updateBounds(x, y, width, height);
+		element.update(x, y, width, height);
+		for(GUIElement child : element.getChildren()) {
+			updateBounds(child, width, height);
+		}
 	}
 }
