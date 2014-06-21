783acc578170e98cbc32480d0eec0970685e32aa
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-21 13:05:33 +0200

    Structure update of the container element

---
+++ ORRE-v2/src/orre/gui/elementNodes/ContainerElementNode.java
@@ -0,0 +1,12 @@
+package orre.gui.elementNodes;
+
+import orre.gui.baseNodes.GUIBaseNode;
+
+public class ContainerElementNode extends GUIBaseNode {
+
+	@Override
+	protected void draw(double x1, double y1, double x2, double y2) {
+
+	}
+
+}
--- ORRE-v2/src/orre/gui/elements/Container.java
+++ ORRE-v2/src/orre/gui/elements/Container.java
@@ -1,12 +1,12 @@
 package orre.gui.elements;
 
 import orre.gui.Bounds;
-import orre.sceneGraph.CoordinateNode;
+import orre.gui.elementNodes.ContainerElementNode;
 
 public class Container extends GUIElement {
 
 	public Container(Bounds bounds, String name) {
-		super(bounds, new CoordinateNode(), name);
+		super(bounds, new ContainerElementNode(), name);
 	}
 
 }
