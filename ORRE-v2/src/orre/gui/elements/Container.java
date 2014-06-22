0fb8ff695205116f81c01715670cba8887eebce3
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-22 13:51:45 +0200

    The container now places itself where it's supposed to.

--- ORRE-v2/src/orre/gui/elements/Container.java
+++ ORRE-v2/src/orre/gui/elements/Container.java
@@ -2,11 +2,15 @@ package orre.gui.elements;
 
 import orre.gui.Bounds;
 import orre.gui.elementNodes.ContainerElementNode;
+import orre.resources.ResourceCache;
 
 public class Container extends GUIElement {
 
 	public Container(Bounds bounds, String name) {
 		super(bounds, new ContainerElementNode(), name);
 	}
+
+	@Override
+	public void initGraphics(ResourceCache resourceCache) {}
 
 }
