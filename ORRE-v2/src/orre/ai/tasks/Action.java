412fcff38b8f8b74432beb193be529ac6153065c
Bart van Blokland <bartiver@stud.ntnu.no>
2014-07-03 21:30:51 +0200

    Implemented Plan

--- ORRE-v2/src/orre/ai/tasks/Action.java
+++ ORRE-v2/src/orre/ai/tasks/Action.java
@@ -1,4 +1,10 @@
 package orre.ai.tasks;
 
 public abstract class Action {
+
+	public abstract boolean isExecutionPossible();
+	public abstract void update();
+	public abstract boolean isFinished();
+	public abstract double getCost();
+	
 }
--- ORRE-v2/src/orre/ai/tasks/Plan.java
+++ ORRE-v2/src/orre/ai/tasks/Plan.java
@@ -1,8 +1,35 @@
 package orre.ai.tasks;
 
-public abstract class Plan {
-	public abstract boolean isExecutionPossible();
-	public abstract void update();
-	public abstract boolean isFinished();
-	public abstract double getPlanCost();
+public final class Plan {
+	private int activeAction = 0;
+	private final Action[] actions;
+	
+	public Plan(Action[] actions) {
+		this.actions = actions;
+	}
+	
+	public boolean isExecutionPossible() {
+		for(Action action : actions) {
+			if(action.isExecutionPossible()) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public void update() {
+		this.actions[activeAction].update();
+	}
+	
+	public boolean isFinished() {
+		return ((activeAction == actions.length - 1) && (actions[activeAction].isFinished()));
+	}
+	
+	public double getPlanCost() {
+		double costSum = 0;
+		for(Action action : actions) {
+			costSum += action.getCost();
+		}
+		return costSum;
+	}
 }
