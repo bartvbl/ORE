46a64d53ad2b8aa88faf2fec111dc23cbdface4e
Bart van Blokland <bartiver@stud.ntnu.no>
2014-07-05 14:11:09 +0200

    Fixed buildings being placed when still holding the build button

--- ORRE-v2/src/orre/input/CommandDispatcher.java
+++ ORRE-v2/src/orre/input/CommandDispatcher.java
@@ -11,6 +11,7 @@ public class CommandDispatcher {
 
 	private final GameWorld world;
 	private final HashMap<String, ArrayList<Integer>> commandMap;
+	private final ArrayList<Runnable> postponedTaskList = new ArrayList<Runnable>();
 
 	public CommandDispatcher(final GameWorld world) {
 		this.world = world;
@@ -31,7 +32,7 @@ public class CommandDispatcher {
 			//handlers are intended to only concern themselves; not registering handlers on other objects.
 			//This is assumed to be the case here.
 			ArrayList<Integer> handlers = commandMap.get(command);
-			for(int i = handlers.size() - 1; i >= 0; i--) {
+			for(int i = 0; i < handlers.size(); i++) {
 				int gameObjectID = handlers.get(i);
 				world.dispatchMessage(message, gameObjectID);
 				if(event.isConsumed()) {
@@ -39,19 +40,31 @@ public class CommandDispatcher {
 				}
 			}
 		}
+		for(Runnable postponedTask : postponedTaskList) {
+			postponedTask.run();
+		}
+		postponedTaskList.clear();
 	}
 
 	public void addInputEventListener(final String command, final int gameObjectID) {
-		if(!commandMap.containsKey(command)) {
-			commandMap.put(command, new ArrayList<Integer>());
-		}
-		this.commandMap.get(command).add(gameObjectID);
+		postponedTaskList.add(new Runnable() {
+			public void run() {
+				if(!commandMap.containsKey(command)) {
+					commandMap.put(command, new ArrayList<Integer>());
+				}
+				commandMap.get(command).add(gameObjectID);
+			}
+		});
 	}
-	
-	public void removeInputEventListener(String command, int gameObjectID) {
-		if(commandMap.containsKey(command)) {
-			int objectIndex = commandMap.get(command).indexOf(gameObjectID);
-			commandMap.get(command).remove(objectIndex);
-		}
+
+	public void removeInputEventListener(final String command, final int gameObjectID) {
+		postponedTaskList.add(new Runnable() {
+			public void run() {
+				if(commandMap.containsKey(command)) {
+					int objectIndex = commandMap.get(command).indexOf(gameObjectID);
+					commandMap.get(command).remove(objectIndex);
+				}
+			}
+		});
 	}
 }
