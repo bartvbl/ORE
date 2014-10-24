df4e61944b2e4b65cc13c42b69308ecf7a5d22c7
Bart van Blokland <bartiver@stud.ntnu.no>
2014-10-24 11:45:20 +0200

    Also added easing to rotation

--- ORRE-v2/src/orre/animation/actions/RotationAction.java
+++ ORRE-v2/src/orre/animation/actions/RotationAction.java
@@ -3,6 +3,7 @@ package orre.animation.actions;
 import orre.animation.Animatable;
 import orre.animation.AnimationAction;
 import orre.animation.AnimationActionType;
+import orre.animation.Ease;
 import orre.geom.Axis;
 import orre.sceneGraph.CoordinateNode;
 
@@ -11,18 +12,31 @@ public class RotationAction extends AnimationAction {
 	private final String partName;
 	private final double rotation;
 	private final Axis axis;
+	private Ease ease;
+	private double totalPercentElapsed = 0;
 
 	public RotationAction(String partName, Axis axis, double rotation) {
 		super(AnimationActionType.ROTATE);
 		this.partName = partName;
 		this.rotation = rotation;
 		this.axis = axis;
+		this.ease = Ease.NO_EASE;
+	}
+	
+	public RotationAction(String partName, Axis axis, double rotation, Ease ease) {
+		this(partName, axis, rotation);
+		this.ease = ease;
 	}
 
 	@Override
 	public void update(Animatable target, double percentElapsed, double timeSinceLastUpdate) {
 		CoordinateNode part = target.getModelPartByName(partName);
-		double updatedRotation = rotation * percentElapsed;
+		
+		double previousPercentage = ease.calculateAnimationPercentage(totalPercentElapsed );
+		double currentPercentage = ease.calculateAnimationPercentage(totalPercentElapsed + percentElapsed);
+		double percentageDelta = currentPercentage - previousPercentage;
+		
+		double updatedRotation = rotation * percentageDelta;
 		if(axis == Axis.x) {
 			part.rotate(updatedRotation, 0, 0);
 		} else if(axis == Axis.y) {
@@ -30,6 +44,7 @@ public class RotationAction extends AnimationAction {
 		} else if(axis == Axis.z) {
 			part.rotate(0, 0, updatedRotation);
 		}
+		totalPercentElapsed += percentElapsed;
 	}
 
 }
