df8e9a9acbcddbc8013f25e8e1956cef18f25a67
Bart van Blokland <bartiver@stud.ntnu.no>
2014-11-09 16:07:08 +0100

    Created seaparate class for housing string script event types

---
+++ ORRE-v2/src/orre/scripting/ScriptEventType.java
@@ -0,0 +1,6 @@
+package orre.scripting;
+
+public class ScriptEventType {
+	public static final String GUI_ANIMATION_EVENT_HANDLED = "GUI_AnimationEventHandled";
+	public static final String GUI_ANIMATION_COMPLETE = "GUI_AnimationComplete";
+}
